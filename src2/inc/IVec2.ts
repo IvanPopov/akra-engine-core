ced1eff92dd99fea127fe45a00274b1cfbe53d1d
Igor Karateev <iakarateev@gmail.com>
2012-11-09 21:47:26 +0400

    Vec4 and Vec3 rewrited

---
+++ src2/inc/IVec2.ts
@@ -0,0 +1,11 @@
+#ifndef IVEC2_TS
+#define IVEC2_TS
+
+module akra {
+	export interface IVec2 {
+		x: float;
+		y: float;
+	};
+};
+
+#endif
--- src2/inc/IVec4.ts
+++ src2/inc/IVec4.ts
@@ -1,12 +1,49 @@
 #ifndef IVEC4_TS
 #define IVEC4_TS
 
-module akra.math {
+module akra {
+
+	IFACE(IVec2);
+	IFACE(IVec3);
+
 	export interface IVec4 {
 		x: float;
 		y: float;
 		z: float;
 		w: float;
+
+		set(): IVec4;
+		set(fValue: float): IVec4;
+		set(v4fVec: IVec4): IVec4;
+		set(pArray: float[]): IVec4;
+		set(fValue: float, v3fVec: IVec3): IVec4;
+		set(v2fVec1: IVec2, v2fVec2: IVec2): IVec4;
+		set(v3fVec: IVec3, fValue: float): IVec4;
+		set(fValue1: float, fValue2: float, v2fVec: IVec2): IVec4;
+		set(fValue1: float, v2fVec: IVec2, fValue2: float): IVec4;
+		set(v2fVec: IVec2, fValue1: float, fValue2: float): IVec4;
+		set(fValue1: float, fValue2: float, fValue3: float, fValue4: float): IVec4;
+
+		clear(): IVec4;
+
+		add(v4fVec: IVec4, v4fDestination?: IVec4): IVec4;
+		subtract(v4fVec: IVec4, v4fDestination?: IVec4): IVec4;
+		dot(v4fVec: IVec4): float;
+
+		isEqual(v4fVec: IVec4, fEps?: float): bool;
+		isClear(fEps?: float): bool;
+
+		negate(v4fDestination?: IVec4): IVec4;
+		scale(fScale: float, v4fDestination?: IVec4): IVec4;
+		normalize(v4fDestination?: IVec4): IVec4;
+		length(): float;
+		lengthSquare(): float;
+
+		direction(v4fVec: IVec4, v4fDestination?: IVec4): IVec4;
+
+		mix(v4fVec: IVec4, fA: float, v4fDestination?: IVec4): IVec4;
+
+		toString(): string;
 	};
 };
 
